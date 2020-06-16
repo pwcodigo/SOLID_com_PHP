0aa5290355f7decc969726a1138858c2e947dbe8
pwcodigo <pwcodigo@email.com>
2020-06-16 06:31:12 -0300

    feat: Aula04-primeiroTesteAutomatizado

---
+++ tests/HtmlTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pwcodigo\Solid;
+
+class HtmlTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEsteEUmSimplesExemploDeTeste(){
+        $this->asserttrue(true);
+    }
+
+}
+?>
